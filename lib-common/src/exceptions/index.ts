38e22020f9766dd75051a9e9bdd1b68247889bdc
birdgang <birdgang82@gmail.com>
2024-02-28 11:29:05 +0900

    update common module exception

--- lib-common/src/exceptions/index.ts
+++ lib-common/src/exceptions/index.ts
@@ -1,6 +1,5 @@
 export * from './business-exception';
-export * from './business-rpc-exception';
 export * from './client-http-exception';
-export * from './client-rpc-exception';
+export * from './custom-rpc-exception';
 export * from './http-exception.filter';
 export * from './rpc-exception.filter';
--- lib-common/src/filters/http-exception.filter.ts
+++ lib-common/src/filters/http-exception.filter.ts
@@ -1,19 +1,24 @@
-import { ExceptionFilter, Catch, ArgumentsHost, HttpException } from '@nestjs/common';
+import {
+  ExceptionFilter,
+  Catch,
+  ArgumentsHost,
+  HttpException,
+} from '@nestjs/common';
 import { Response } from 'express';
 
 @Catch(HttpException)
 export class HttpExceptionFilter implements ExceptionFilter {
-    catch(exception: HttpException, host: ArgumentsHost) {
-        const ctx = host.switchToHttp();
-        const response = ctx.getResponse<Response>();
-        const status = exception.getStatus();
-        const exceptionResponse = exception.getResponse();
+  catch(exception: HttpException, host: ArgumentsHost) {
+    const ctx = host.switchToHttp();
+    const response = ctx.getResponse<Response>();
+    const status = exception.getStatus();
+    const exceptionResponse = exception.getResponse();
 
-        response
-            .status(status)
-            .json({
-                statusCode: status,
-                ...(typeof exceptionResponse === 'object' ? exceptionResponse : { message: exceptionResponse }),
-            });
-    }
+    response.status(status).json({
+      statusCode: status,
+      ...(typeof exceptionResponse === 'object'
+        ? exceptionResponse
+        : { message: exceptionResponse }),
+    });
+  }
 }
