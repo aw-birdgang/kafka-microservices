c9c2a2bb2727bb9f050753a7ca28d58917316555
birdgang <birdgang82@gmail.com>
2024-02-28 18:49:13 +0900

    update game msa module

--- game-microservice/src/app.module.ts
+++ game-microservice/src/app.module.ts
@@ -3,14 +3,18 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 
 import { DataValidationModule, LanguageModule } from '@birdgang/lib-common';
+import { FinanceService } from './api/finance/finance.service';
+import { FinanceController } from './api/finance/finance.controller';
+import { FinanceModule } from './api/finance/finance.module';
 
 @Module({
   imports: [
     forwardRef(() => LanguageModule),
     forwardRef(() => DataValidationModule),
+    FinanceModule,
   ],
-  controllers: [AppController],
-  providers: [AppService],
+  controllers: [AppController, FinanceController],
+  providers: [AppService, FinanceService],
   // providers: [
   //   {
   //     provide: APP_FILTER,
